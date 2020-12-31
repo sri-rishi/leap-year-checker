b917b7c07e0d79cba2ab0f0776d5365dbccdb715
Rishi Srivastava <72429483+sri-rishi@users.noreply.github.com>
2021-01-01 00:35:49 +0530

    created app.js
    added app.js to initial

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  var [value, setValue] = useState("");
+  var [greet, setGreet] = useState("");
+  var [error, setError] = useState("");
+
+  function inputHandler(event) {
+    var userInput = event.target.value;
+    value = userInput;
+    setValue(value);
+  }
+
+  function clickHandler() {
+    if (isNaN(value)) {
+      setError("Error: You have entered wrong type");
+    } else {
+      if ((value % 4 === 0 && value % 100 !== 0) || value % 400 === 0) {
+        setGreet("Yep! Born in leap year");
+      } else {
+        setGreet("Sorry! not a leap year");
+      }
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>leap Year Checker</h1>
+      <input onChange={inputHandler} />
+      <button onClick={clickHandler}>Check</button>
+      <div className="error">{error}</div>
+      <div className="greet">{greet}</div>
+
+      <footer>
+        <div>
+          <p className="about">About</p>
+          <p>
+            Have you ever wonder that year you have been born might be a leap
+            year. So, you can check now by just entering your birth year.
+          </p>
+          <p>
+            So, check and have fun with <strong>Leap Year Checker</strong>
+          </p>
+          <p>
+            Made by{" "}
+            <a href="https://rishiportfolio.netlify.app">Rishi Srivastav</a>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
